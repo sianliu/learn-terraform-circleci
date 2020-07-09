21fca503e3c31d0ee9adea1e0b9ae22287cc2343
Jonathan Liu <sianliu@gmail.com>
2020-07-09 09:03:18 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "<YOUR-ORG-NAME>"
+    organization = "wallex-hashicorp"
 
     workspaces {
       name = "learn-terraform-circleci"
